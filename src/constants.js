116777df3ea095da50c6a7a851d66c1759a1c6c6
bikrant jajware <bikrantjajware@gmail.com>
2023-09-17 19:47:36 +0530

    add Nike landing project

---
+++ src/constants.js
@@ -0,0 +1,16 @@
+import { m3mories, nikeLanding } from './images/projects';
+
+export const PROJECTS = [
+  {
+    'title': 'Archives',
+    'description': 'a digital diary for your journals',
+    'link': 'https://m3mories.netlify.app/',
+    'imgUrl': m3mories
+  },
+  {
+    'title': 'Nike Landing',
+    'description': 'a simple landing page made with tailwindcss',
+    'link': 'https://silver-jalebi.netlify.app/',
+    'imgUrl': nikeLanding
+  }
+];
--- src/images/projects/index.js
+++ src/images/projects/index.js
@@ -1,3 +1,4 @@
 import m3mories from './m3mories.png';
+import nikeLanding from './nike-landing.png';
 
-export { m3mories };
+export { m3mories, nikeLanding };
