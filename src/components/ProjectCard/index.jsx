6a427574ff653445a4e5b1e0a05877d3d9353207
bikrant jajware <bikrantjajware@gmail.com>
2023-09-17 19:47:55 +0530

    tinker with style

--- src/components/ProjectCard/index.jsx
+++ src/components/ProjectCard/index.jsx
@@ -10,19 +10,19 @@ const ProjectCard = ({ title, imgUrl, description, link }) => {
       onMouseLeave={() => setIsHovered(false)}
       className={`relative rounded-lg overflow-hidden transition-transform transform hover:-translate-y-1 hover:shadow-lg ${
         isHovered
-          ? 'bg-opacity-80 border-4 border-blue-400'
-          : 'bg-opacity-100 border-4 border-gray-300'
+          ? 'bg-opacity-80 border-4 border-red-400'
+          : 'bg-opacity-100 border-4 border-violet-300'
       }`}>
       <img src={imgUrl} className="object-cover" alt={title} />
       {isHovered && (
-        <div className="absolute inset-0 flex flex-col items-center justify-center p-4 text-white bg-black bg-opacity-80">
-          <h3 className="text-xl font-semibold">{title}</h3>
-          <p className="mt-2 text-sm text-sky-300">{description}</p>
+        <div className="absolute inset-0 flex flex-col items-center justify-center p-4 text-slate-200 bg-blue-950 bg-opacity-80">
+          <h3 className="text-4xl font-semibold">{title}</h3>
+          <p className="mt-2 text-lg text-sky-300">{description}</p>
           <a
             href={link}
             target="_blank"
             rel="noopener noreferrer"
-            className="mt-4 hover:text-blue-400 text-blue-500 hover:underline">
+            className="mt-4 hover:text-green-400 text-green-500 hover:underline">
             View Project
           </a>
         </div>
