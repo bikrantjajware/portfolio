7b265a416af5c9fdb78aec1a0059f9debd168cfe
bikrant jajware <bikrantjajware@gmail.com>
2023-09-19 14:24:06 +0530

    update footer style

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <div className="h-40 mt-6 bg-slate-950 relative">
-      <p className="text-slate-50 font-palanquin text-center absolute bottom-0 left-[50%] translate-x-[-50%]">
+    <div className="h-40 mt-6 relative">
+      <p className=" font-palanquin text-center absolute bottom-0 left-[50%] translate-x-[-50%]">
         made with ♥️ by <span>bikrant jajware</span>
       </p>
     </div>
