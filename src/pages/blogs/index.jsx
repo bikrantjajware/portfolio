59fef6d777a6cfb54403c5cf16f43fc9ebe97141
bikrant jajware <bikrantjajware@gmail.com>
2023-09-21 20:00:14 +0530

    fix lint and minor css changes

--- src/pages/blogs/index.jsx
+++ src/pages/blogs/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useState } from 'react';
-import './style.css';
 
 const Blogs = () => {
   const [title, setTitle] = useState('');
--- src/pages/projects/index.jsx
+++ src/pages/projects/index.jsx
@@ -5,7 +5,7 @@ import { PROJECTS } from '../../constants';
 const Projects = () => {
   return (
     <div>
-      <h3 className="py-3 text-4xl font-medium"> Projects</h3>
+      <h3 className="py-3 text-4xl font-palanquin"> Projects</h3>
       <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-3 mx-auto">
         {PROJECTS.map((project) => (
           <ProjectCard {...project} />
