1f605ba94cb670c93f250534b57d2702129698a7
bikrant jajware <bikrantjajware@gmail.com>
2023-09-30 16:29:46 +0530

    css fixes

--- src/pages/projects/index.jsx
+++ src/pages/projects/index.jsx
@@ -6,7 +6,7 @@ const Projects = () => {
   return (
     <div>
       <h3 className="py-3 text-4xl font-palanquin"> Projects</h3>
-      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-3 mx-auto">
+      <div className="grid md:grid-cols-2  gap-3 mx-auto">
         {PROJECTS.map((project, i) => (
           <ProjectCard key={i} {...project} />
         ))}
--- src/sections/Header/index.jsx
+++ src/sections/Header/index.jsx
@@ -6,7 +6,7 @@ const Header = () => {
   return (
     <div>
       <h3 className="text-4xl font-palanquin py-3"> I'm Bikrant</h3>
-      <h3 className="text-2xl font-montserrat py-1 pb-3 text-slate-600">
+      <h3 className="text-2xl font-montserrat py-1 pb-3 ">
         your friendly remote
         <span className="text-orange-700 font-medium"> Software Developer</span>
       </h3>
@@ -17,7 +17,7 @@ const Header = () => {
       </div>
       <div className="py-3 md:w-[70%] ">
         <h3 className="py-3 text-4xl font-palanquin"> A little about me</h3>
-        <p className="font-montserrat text-xl text-slate-800">
+        <p className="font-montserrat text-xl ">
           I'm a CS graduate with <strong>4+</strong> years experience of working in software space.
           I've experience working on <span className="text-orange-700 font-medium"> MERN</span>{' '}
           stack, and <span className="text-orange-700 font-medium"> AWS serverless </span>
