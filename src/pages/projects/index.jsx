27712886e307659eff8d86d640672485c8f86634
Bikrant Jajware <bikrantjajware@gmail.com>
2024-06-20 23:28:54 +0530

    add some basic styles in homepage

--- src/pages/projects/index.jsx
+++ src/pages/projects/index.jsx
@@ -4,14 +4,16 @@ import { PROJECTS } from '../../constants';
 
 const Projects = () => {
   return (
-    <div>
-      <h3 className="py-3 text-4xl font-palanquin"> Projects</h3>
-      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-3 mx-auto">
-        {PROJECTS.map((project, i) => (
-          <ProjectCard key={i} {...project} />
-        ))}
+    <section class="bg-red-100 rounded-lg p-2 hover:bg-red-200 my-2">
+      <div class="dark:text-slate-300">
+        <h3 className="py-3 text-4xl font-palanquin"> Projects</h3>
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-3 mx-auto">
+          {PROJECTS.map((project, i) => (
+            <ProjectCard key={i} {...project} />
+          ))}
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 
--- src/sections/Header/index.jsx
+++ src/sections/Header/index.jsx
@@ -4,26 +4,32 @@ import { FaSquareXTwitter } from 'react-icons/fa6';
 
 const Header = () => {
   return (
-    <div>
-      <h3 className="text-4xl font-palanquin py-3"> I'm Bikrant</h3>
-      <h3 className="text-2xl font-montserrat py-1 pb-3 text-slate-600">
-        your friendly remote
-        <span className="text-orange-700 font-medium"> Software Developer</span>
-      </h3>
-      <div className="flex flex-row gap-5 border-red-100 rounded-lg">
-        <FaLinkedin size="35" />
-        <FaGithubSquare size="35" />
-        <FaSquareXTwitter size="35" />
-      </div>
-      <div className="py-3 w-[60%] ">
-        <h3 className="py-3 text-4xl font-palanquin"> A little about me</h3>
-        <p className="font-montserrat text-xl text-slate-800">
-          I'm a CS graduate with <strong>4+</strong> years experience of working in software space.
-          I've experience working on <span className="text-orange-700 font-medium"> MERN</span>{' '}
-          stack, and <span className="text-orange-700 font-medium"> AWS serverless </span>
-          stack.
-        </p>
-      </div>
+    <div class="dark:text-slate-300">
+      <section class="bg-orange-100 rounded-lg p-2 hover:bg-orange-200 my-2">
+        <h3 className="text-4xl font-palanquin py-2"> I'm Bikrant</h3>
+        <h3 className="text-2xl font-montserrat py-1 pb-3 text-slate-600 dark:text-slate-400">
+          your friendly remote
+          <span className="text-orange-700 font-medium"> Software Developer</span>
+        </h3>
+        <div className="flex flex-row gap-5 border-red-100 rounded-lg">
+          <FaLinkedin size="35" />
+          <FaGithubSquare size="35" />
+          <FaSquareXTwitter size="35" />
+        </div>
+      </section>
+
+      <section class="bg-blue-100 rounded-lg p-2 hover:bg-blue-200 my-2">
+        <div className="py-3 md:w-[60%]">
+          <h3 className="py-3 text-4xl font-palanquin"> A little about me</h3>
+          <p className="font-montserrat text-xl text-slate-800 dark:text-slate-400">
+            I'm a CS graduate with <strong>4+</strong> years experience of working in software
+            space. I've experience working on{' '}
+            <span className="text-orange-700 font-medium"> MERN</span> stack, and{' '}
+            <span className="text-orange-700 font-medium"> AWS serverless </span>
+            stack.
+          </p>
+        </div>
+      </section>
     </div>
   );
 };
