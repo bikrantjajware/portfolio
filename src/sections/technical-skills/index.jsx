f939b17a9dab586c73241fbee758cca55cde1fc2
Bikrant Jajware <bikrantjajware@gmail.com>
2024-06-28 15:04:00 +0530

    modify style for technical skills section

--- src/sections/technical-skills/index.jsx
+++ src/sections/technical-skills/index.jsx
@@ -16,58 +16,64 @@ import Section from '../../components/section';
 const TechnicalSkills = () => {
   return (
     <Section className="bg-lime-50">
-      <div>
-        <h3 className="py-3 text-3xl font-palanquin"> Skills in Tech</h3>
-        <h4 className="text-xl font-medium font-palanquin"> Progarmming Languages</h4>
-        <Section className="bg-gray-100 flex gap-3 shadow-lg w-fit">
-          <div>
-            <img className="skill-icon" height="80px" src={CplusplusIcon} alt="c++" />
-            <span>c++</span>
-          </div>
-          <div>
-            <img className="skill-icon" height="80px" src={javascriptIcon} alt="javascript" />
-            <span>javascript</span>
-          </div>
-          <div>
-            <img className="skill-icon" height="80px" src={typescriptIcon} alt="typescript" />
-            <span>typescript</span>
+      <h3 className="py-3 text-3xl font-palanquin"> Skills in Tech</h3>
+      <div className="flex gap-x-5 flex-wrap">
+        <Section className="bg-gray-100 flex flex-col gap-2 shadow-lg max-w-fit">
+          <h4 className="text-xl font-medium font-palanquin"> Progarmming Languages</h4>
+          <div className="flex gap-3">
+            <div className="text-center">
+              <img className="skill-icon" height="80px" src={CplusplusIcon} alt="c++" />
+              <span>C++</span>
+            </div>
+            <div className="text-center">
+              <img className="skill-icon" height="80px" src={javascriptIcon} alt="javascript" />
+              <span>Javascript</span>
+            </div>
+            <div className="text-center">
+              <img className="skill-icon" height="80px" src={typescriptIcon} alt="typescript" />
+              <span>Typescript</span>
+            </div>
           </div>
         </Section>
-        <h4 className="text-xl font-medium font-palanquin">Database</h4>
-        <Section className="bg-gray-100 flex gap-3 shadow-lg w-fit">
-          <div>
-            <img className="skill-icon" height="80px" src={mysqlIcon} alt="mysql" />
-            <span>MySQL</span>
-          </div>
-          <div>
-            <img className="skill-icon" height="80px" src={dynamodbIcon} alt="dynamodb" />
-            <span>Dynamo DB</span>
-          </div>
-          <div>
-            <img className="skill-icon" height="80px" src={mongodbIcon} alt="mongodb" />
-            <span>Mongo DB</span>
+        <Section className="bg-gray-100 flex flex-col gap-2 shadow-lg max-w-fit">
+          <h4 className="text-xl font-medium font-palanquin">Database</h4>
+          <div className="flex gap-3">
+            <div>
+              <img className="skill-icon" height="80px" src={mysqlIcon} alt="mysql" />
+              <span>MySQL</span>
+            </div>
+            <div>
+              <img className="skill-icon" height="80px" src={dynamodbIcon} alt="dynamodb" />
+              <span>{'Dynamo DB '}</span>
+            </div>
+            <div>
+              <img className="skill-icon" height="80px" src={mongodbIcon} alt="mongodb" />
+              <span>Mongo DB</span>
+            </div>
           </div>
         </Section>
-        <h4 className="text-xl font-medium font-palanquin">Framework / Technologies</h4>
-        <Section className="bg-gray-100 flex gap-3 shadow-lg w-fit">
-          <div className="skills">
-            <div>
-              <img
-                className="skill-icon"
-                height="80px"
-                src={serverlessIcon}
-                alt="aws"
-                tooltip="hello"
-              />
-              <span>Serverless</span>
-            </div>
-            <div>
-              <img className="skill-icon" height="80px" src={reactIcon} alt="react" />
-              <span>react</span>
-            </div>
-            <div>
-              <img className="skill-icon" height="80px" src={nodejsIcon} alt="node" />
-              <span>node js</span>
+        <Section className="bg-gray-100 flex flex-col gap-2 shadow-lg max-w-fit">
+          <h4 className="text-xl font-medium font-palanquin">Framework / Technologies</h4>
+          <div className="flex gap-3">
+            <div className="skills">
+              <div>
+                <img
+                  className="skill-icon"
+                  height="80px"
+                  src={serverlessIcon}
+                  alt="aws"
+                  tooltip="hello"
+                />
+                <span>Serverless</span>
+              </div>
+              <div>
+                <img className="skill-icon" height="80px" src={reactIcon} alt="react" />
+                <span>react</span>
+              </div>
+              <div>
+                <img className="skill-icon" height="80px" src={nodejsIcon} alt="node" />
+                <span>node js</span>
+              </div>
             </div>
           </div>
         </Section>
